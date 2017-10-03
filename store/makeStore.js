12ee822b762db932be1c04d603b1313adba739db
Enrique Benitez <enriquebenitezjr@gmail.com>
2017-10-02 21:48:32 -0500

    Remove Redux stuff for now

--- store/makeStore.js
+++
@@ -1,9 +0,0 @@
-import { createStore, combineReducers } from 'redux'
-import devToolsEnhancer from 'remote-redux-devtools'
-
-const makeStore = (initialState = {}) => {
-  const reducers = combineReducers({})
-  return createStore(reducers, initialState, devToolsEnhancer())
-}
-
-export default makeStore
