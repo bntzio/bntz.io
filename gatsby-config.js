72cd423906a1f44e6cdaa0b91d29031ad9e5796f
Enrique Benitez <enriquebenitezjr@gmail.com>
2018-03-18 23:41:38 -0600

    Remove unnecessary stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: 'Superstylin',
-    description: 'A Gatsby Starter with styled-components'
+    title: 'Enrique Benitez',
+    description: 'Full-Stack Developer and Maker'
   },
   plugins: [
     'gatsby-plugin-styled-components',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/posts`,
-        name: 'posts'
-      }
-    },
-    'gatsby-transformer-remark',
     'gatsby-plugin-react-helmet'
   ]
 }
