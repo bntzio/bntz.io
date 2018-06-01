59a82887b4971c328010114e86697262b18e3617
Enrique Benitez <enriquebenitezjr@gmail.com>
2018-06-01 01:49:16 -0500

    Add stylesheet link

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -30,6 +30,10 @@ const TemplateWrapper = ({ children }) => (
         {
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css?family=Work+Sans:300,400,500'
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://cloud.typography.com/7471216/6094192/css/fonts.css'
         }
       ]}
     />
