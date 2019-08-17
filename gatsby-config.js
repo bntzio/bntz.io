354c57b503bb826d85fe5a3e30b4efc659b78266
Enrique Benitez <enriquebenitezjr@gmail.com>
2019-08-17 17:56:50 -0500

    Clean repo and install gatsby theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,31 @@
 module.exports = {
   siteMetadata: {
-    title: 'Enrique Benitez',
-    description: 'Full-Stack Developer and Maker'
+    title: `Enrique Benitez`,
+    name: `bntz.io`,
+    siteUrl: `https://bntz.io`,
+    description: `Enrique Benitez is a full-stack developer and maker.`,
+    hero: {
+      heading: `Hi! 👋 Welcome to my blog.`,
+      maxWidth: 652,
+    },
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/bntzio`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/bntzio`,
+      },
+      {
+        name: `instagram`,
+        url: `https://instagram.com/bntzio`,
+      },
+    ],
   },
   plugins: [
-    'gatsby-plugin-styled-components',
-    'gatsby-plugin-react-helmet'
-  ]
-}
+    {
+      resolve: "@narative/gatsby-theme-novela",
+    },
+  ],
+};
--- gatsby-node.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-
-exports.onCreateWebpackConfig = ({ stage, actions }) => {
-  actions.setWebpackConfig({
-    resolve: {
-      modules: [path.resolve(__dirname, 'src'), 'node_modules']
-    }
-  })
-}
