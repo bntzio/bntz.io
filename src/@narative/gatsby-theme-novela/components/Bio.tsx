150816ccded6d8294ef2be78393b943acd6e669d
Enrique Benitez <enriquebenitezjr@gmail.com>
2019-08-18 19:30:58 -0500

    Add typescript support and shadow theme

---
+++ src/@narative/gatsby-theme-novela/components/Bio.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+export default () => <BioContainer>Enrique Benitez</BioContainer>
+
+const BioContainer = styled.div`
+  background: yellow;
+  display: flex;
+  align-items: center;
+  position: relative;
+  left: -10px;
+`
